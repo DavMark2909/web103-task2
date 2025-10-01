fdc24619cde876441783ad94915be44db10805c3
DavMark2909 <77192509+DavMark2909@users.noreply.github.com>
2025-09-30 23:46:12 -0400

    Create music.js

---
+++ server/data/music.js
@@ -0,0 +1,54 @@
+const musicData = [
+  {
+    "id": 1,
+    "eventName": "Night show",
+    "artists": "MoneyMaker",
+    "date": "09-29",
+    "venue": "San Tropez",
+    "genre": "House",
+    "price": "75.00",
+    "image": "https://cdm0lfbn.cloudimg.io/v7/_images_base_/image_uploader/photos/a0/original/les-caves-du-roy-nightclub-saint-tropez-interior.jpg"
+  },
+  {
+    "id": 2,
+    "eventName": "Bye bye",
+    "artists": "AltzMuller",
+    "date": "10-16",
+    "venue": "Berlin",
+    "genre": "Rave",
+    "price": "23.00",
+    "image": "https://media.gettyimages.com/id/1526102254/photo/rave-the-planet-techno-parade-in-berlin.jpg?s=2048x2048&w=gi&k=20&c=07QdDn9e5VjcGb-Vy0r6XfrQvJ3UDXns_z4tGOxVLSY="
+  },
+  {
+    "id": 3,
+    "eventName": "MoneyWorld",
+    "artists": "Monclerc",
+    "date": "02-25",
+    "venue": "Monaco",
+    "genre": "House",
+    "price": "300.00",
+    "image": "https://media.gettyimages.com/id/597274327/photo/elevated-view-of-superyachts-at-monaco-yacht-show-at-dusk.jpg?s=2048x2048&w=gi&k=20&c=inroOQRgQHtT-DQ1cKLEIUKLm_nxXcxWglgqtEeVcaQ="
+  },
+  {
+    "id": 4,
+    "eventName": "2017",
+    "artists": "Kievstoner",
+    "date": "05-05",
+    "venue": "",
+    "genre": "Hip-Hop",
+    "price": "228.00",
+    "image": "https://24smi.org/public/media/290x360/celebrity/2025/09/17/dzwrahlrzkxx-kievstoner.jpg"
+  },
+  {
+    "id": 5,
+    "eventName": "MoetChandon",
+    "artists": "Morgenstern",
+    "date": "06-06",
+    "venue": "Prague",
+    "genre": "Hip-Hop",
+    "price": "73.00",
+    "image": "https://imgs.search.brave.com/XG_xXct6suPYYA52BRPRSsYPapUtzmkUZ2T3MOPOvbc/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly91cGxv/YWQud2lraW1lZGlh/Lm9yZy93aWtpcGVk/aWEvY29tbW9ucy90/aHVtYi8zLzMwLyVE/MCU5MCVEMCVCQiVE/MCVCOCVEMSU4OCVE/MCVCNSVEMSU4MF8l/RDAlOUMlRDAlQkUl/RDElODAlRDAlQjMl/RDAlQjUlRDAlQkQl/RDElODglRDElODIl/RDAlQjUlRDElODAl/RDAlQkRfJTI4Y3Jv/cHBlZCUyOS5qcGcv/NTEycHgtJUQwJTkw/JUQwJUJCJUQwJUI4/JUQxJTg4JUQwJUI1/JUQxJTgwXyVEMCU5/QyVEMCVCRSVEMSU4/MCVEMCVCMyVEMCVC/NSVEMCVCRCVEMSU4/OCVEMSU4MiVEMCVC/NSVEMSU4MCVEMCVC/RF8lMjhjcm9wcGVk/JTI5LmpwZw"
+  }
+]
+
+export default musicData;
